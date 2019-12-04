3cb53fdae99f77dd23b716fb7c17f41a5b978b06
gevel <gevelencko8@gmail.com>
2019-12-04 13:21:53 +0200

    [add]: Test for PersonaTaxRecorder

---
+++ Task1/src/test/java/TestingTaxRecorder.java
@@ -0,0 +1,25 @@
+import Model.PersonalTaxRecorder;
+import Model.User.Property;
+import Model.User.User;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestingTaxRecorder {
+    static User user;
+    static PersonalTaxRecorder personalTaxRecorder;
+
+    @BeforeClass
+    public static void runner() {
+        user = new User(70000, 3, 8000, Property.values());
+        personalTaxRecorder = new PersonalTaxRecorder(user);
+    }
+
+    @Test
+    public void getFullTaxToPayTest() {
+        double expected = 12444 + 1156 + 13005 + 10115 + 72.25;
+        double real = personalTaxRecorder.getFullTaxToPay();
+        Assert.assertEquals(expected, real, 0.00000001);
+    }
+
+}
