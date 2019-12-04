b53a8abbdbb5c1caed11085816dbfd5341025f15
gevel <gevelencko8@gmail.com>
2019-12-04 13:21:18 +0200

    [Update]: now tests are with new property type

--- Task1/src/test/java/TestingPropertyTax.java
+++ Task1/src/test/java/TestingPropertyTax.java
@@ -1,4 +1,5 @@
 import Model.Taxes.PropertyTax;
+import Model.User.Property;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -7,32 +8,32 @@ public class TestingPropertyTax {
     static PropertyTax propertyTax;
     @BeforeClass
     public static void runner(){
-        propertyTax = new PropertyTax(3700,0,8000,"Car");
+        propertyTax = new PropertyTax(3700,0, Property.CAR);
     }
 
     @Test
     public void hashCodeTest(){
-        int expected = 5857700;
+        int expected = 5929700;
         int real = propertyTax.hashCode();
         Assert.assertEquals(expected,real);
     }
 
     @Test
     public void equalsTestForTrue(){
-        PropertyTax propertyTax1 = new PropertyTax(3700,0,8000, "Car");
+        PropertyTax propertyTax1 = new PropertyTax(3700,0,Property.CAR);
         Assert.assertTrue(propertyTax1.equals(propertyTax));
     }
 
     @Test
     public void equalsTestForFalse(){
-        PropertyTax propertyTax1 = new PropertyTax(3700,1,8000, "Car");
+        PropertyTax propertyTax1 = new PropertyTax(3700,1, Property.CAR);
         Assert.assertFalse(propertyTax1.equals(propertyTax));
     }
 
     @Test
     public void countTaxToPayTest(){
-        double expected = 1224.0;
+        double expected = 12240.0;
         double real = propertyTax.getTaxToPay();
-        Assert.assertEquals(expected, real, 0);
+        Assert.assertEquals(expected, real, 0.0000001);
     }
 }
